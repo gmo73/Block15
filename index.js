16acad3d0a1970c8164eaf8327a20c05ecece866
gmo73 <monroygmo73@gmail.com>
2023-10-04 22:15:43 -0500

    initial

---
+++ index.js
@@ -0,0 +1,51 @@
+// ser to input inceream falvors seperated by commas
+const userInputString = prompt(
+    "Please enter froyo flavors seperated by commas: ",
+    "chocolate,vanilla,strawberry,coffee"
+);
+
+
+// Split the string of inputs into an array of strings
+const flavorsArray = userInputString.split(",");
+
+
+
+const flavors = [];
+for (let i = 0; i < flavorsArray.length; i++) {
+ const str= flavorsArray[i];
+}
+
+
+
+function falvors (flavorsYog) {
+    let flavors = Vanilla[0] + Strawberry[0] + Coffee[0] + Chocolate[0]
+
+   for (let i = 0; i < flavorsArray.length; i++){
+     
+
+         if (flavors[i]  ='Vanilla'){
+            flavorsYog.Vanilla += 1;
+         }
+         else if (flavors[i] = 'chocolate'){
+            flavorsYog.Chocolate += 1;
+         }
+         else if (flavors [i]= 'strawberry') {
+            flavorsYog.Strawberry += 1;
+         }
+         else if (flavors [i]= 'coffee') {
+            flavorsYog .Coffee += 1;
+         }            
+        }
+        return flavors;
+    }
+       
+       
+ 
+    
+
+ 
+
+
+ 
+
+
